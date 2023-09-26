d4d16f7c1543c68f5928618350ae2acc8711398c
Dhruv Tanna <dhtann@microsoft.com>
2023-09-27 01:01:05 +0530

    insert node and print link list programs added

---
+++ HackerRank/Solutions/InsertNodeAtTail.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+
+namespace HackerRank.Solutions
+{
+    public class InsertNodeAtTail
+    {
+        // https://www.hackerrank.com/challenges/insert-a-node-at-the-tail-of-a-linked-list/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+
+        class SinglyLinkedListNode
+        {
+            public int data;
+            public SinglyLinkedListNode next;
+
+            public SinglyLinkedListNode(int nodeData)
+            {
+                this.data = nodeData;
+                this.next = null;
+            }
+        }
+
+        class SinglyLinkedList
+        {
+            public SinglyLinkedListNode head;
+
+            public SinglyLinkedList()
+            {
+                this.head = null;
+            }
+
+        }
+
+        static void PrintSinglyLinkedList(SinglyLinkedListNode node, string sep, TextWriter textWriter)
+        {
+            while (node != null)
+            {
+                textWriter.Write(node.data);
+
+                node = node.next;
+
+                if (node != null)
+                {
+                    textWriter.Write(sep);
+                }
+            }
+        }
+
+        internal void TakeInput()
+        {
+            SinglyLinkedList llist = new SinglyLinkedList();
+
+            int llistCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < llistCount; i++)
+            {
+                int llistItem = Convert.ToInt32(Console.ReadLine());
+                SinglyLinkedListNode llist_head = insertNodeAtTail(llist.head, llistItem);
+                llist.head = llist_head;
+            }
+
+            Console.ReadKey();
+        }
+
+        static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data)
+        {
+            if (head == null)
+            {
+                head = new SinglyLinkedListNode(data);
+            }
+            else
+            {
+                var tailNode = new SinglyLinkedListNode(data);
+
+                head = GetLastNode(head, tailNode);                
+            }
+
+            return head;
+        }
+
+        private static SinglyLinkedListNode GetLastNode(SinglyLinkedListNode head, SinglyLinkedListNode tailNode)
+        {
+            if (head.next == null)
+            {
+                head.next = tailNode;
+            }
+            else
+            {
+                head.next = GetLastNode(head.next, tailNode);
+            }
+
+            return head;
+        }
+    }
+}
---
+++ HackerRank/Solutions/PrintLinkedList.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace HackerRank.Solutions
+{
+    public class PrintLinkedList
+    {
+        // https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list/problem?isFullScreen=true
+
+        class SinglyLinkedListNode
+        {
+            public int data;
+            public SinglyLinkedListNode next;
+
+            public SinglyLinkedListNode(int nodeData)
+            {
+                this.data = nodeData;
+                this.next = null;
+            }
+        }
+
+        class SinglyLinkedList
+        {
+            public SinglyLinkedListNode head;
+            public SinglyLinkedListNode tail;
+
+            public SinglyLinkedList()
+            {
+                this.head = null;
+                this.tail = null;
+            }
+
+            public void InsertNode(int nodeData)
+            {
+                SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+                if (this.head == null)
+                {
+                    this.head = node;
+                }
+                else
+                {
+                    this.tail.next = node;
+                }
+
+                this.tail = node;
+            }
+        }
+
+        internal void TakeInput()
+        {
+            SinglyLinkedList llist = new SinglyLinkedList();
+
+            int llistCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < llistCount; i++)
+            {
+                int llistItem = Convert.ToInt32(Console.ReadLine());
+                llist.InsertNode(llistItem);
+            }
+
+            printLinkedList(llist.head);
+        }
+
+        private void printLinkedList(SinglyLinkedListNode head)
+        {
+            if(head == null)
+            {
+                return;
+            }
+            var currentNode = head;
+
+            do
+            {
+                Console.WriteLine(currentNode.data);
+                currentNode = currentNode.next;
+
+            } while (currentNode != null);
+
+        }
+    }
+}
