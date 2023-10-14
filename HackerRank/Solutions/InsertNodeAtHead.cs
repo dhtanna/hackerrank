ae0d45dfd49cb78049745584edb2451eb3a6dd28
dhtanna <dhtanna@outlook.com>
2023-10-14 14:03:42 +0530

    left rotation completed and node at head needs to be started

---
+++ HackerRank/Solutions/InsertNodeAtHead.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace HackerRank.Solutions
+{
+    public class InsertNodeAtHead
+    {
+
+        // https://www.hackerrank.com/challenges/insert-a-node-at-the-head-of-a-linked-list/problem?isFullScreen=true
+
+        internal void TakeInput()
+        {
+            SinglyLinkedList llist = new SinglyLinkedList();
+
+            int llistCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < llistCount; i++)
+            {
+                int llistItem = Convert.ToInt32(Console.ReadLine());
+                SinglyLinkedListNode llist_head = insertNodeAtHead(llist.head, llistItem);
+                llist.head = llist_head;
+            }
+
+            PrintSinglyLinkedList(llist.head);
+        }
+
+        static void PrintSinglyLinkedList(SinglyLinkedListNode node)
+        {
+            while (node != null)
+            {
+                Console.WriteLine(node.data);
+
+                node = node.next;                
+            }
+        }
+
+        SinglyLinkedListNode insertNodeAtHead(SinglyLinkedListNode llist, int data)
+        {
+            SinglyLinkedListNode result = null;
+
+            return result;
+
+        }
+    }
+
+    class SinglyLinkedListNode
+    {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData)
+        {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    class SinglyLinkedList
+    {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+        }
+
+    }
+
+    
+}
---
+++ HackerRank/Solutions/LeftRotation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRank.Solutions
+{
+    public class LeftRotation
+    {
+        // https://www.hackerrank.com/challenges/array-left-rotation/problem?isFullScreen=true
+
+        internal void TakeInput()
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(firstMultipleInput[0]);
+
+            int d = Convert.ToInt32(firstMultipleInput[1]);
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            List<int> result = rotateLeft(d, arr);
+
+            Console.WriteLine(String.Join(" ", result));
+
+            Console.ReadKey();
+        }
+
+        private List<int> rotateLeft(int d, List<int> arr)
+        {
+            List<int> extractedNumbers = new List<int>();
+            int j = 0;
+            int swapped = 0;
+            for (int i = 0; i < arr.Count; i++)
+            {
+                if (i < d)
+                {
+                    extractedNumbers.Add(arr[i]);
+                }
+                else
+                {
+                    swapped++;
+                    arr[j++] = arr[i];
+                }
+            }
+
+            j = 0;
+
+            for (int i = swapped; i < arr.Count; i++)
+            {
+                arr[i] = extractedNumbers[j++];
+            }
+
+            return arr;
+        }
+    }
+}
